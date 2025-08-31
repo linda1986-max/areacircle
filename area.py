136ee5271504be72b2fc703231306e14960aa034
lee bee dell <lindabrendamidzi@gmail.com>
2025-08-31 10:41:03 +0200

    Assignment

---
+++ area.py
@@ -0,0 +1,12 @@
+# Define a constant (by convention, constants are written in uppercase)
+PI = 3.14159
+
+# Define variables
+radius = 5
+area = PI * radius * radius
+
+# Use the print function to display results
+print("Radius of the circle:", radius)
+print("Area of the circle:", area)
+print("Test")
+
